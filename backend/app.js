130631afec6b7d21e373ad227d44ed3d72a0ef96
skwak22 <skwak22@amherst.edu>
2021-02-13 11:32:00 -0500

    POST requests for zip, news, and officials

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ function handleError(res, reason, message, code) {
 function getDocuments(snapshot) {
   let tmp = {};
   snapshot.forEach((doc) => {
-    tmp[doc.id] = doc.data();
+  tmp[doc.id] = doc.data();
   });
   return tmp;
 }
@@ -54,43 +54,131 @@ var server = app.listen(port, hostname, () => {
  */
 app.get("/api/officials", function (req, res) {
   db.collection('elected_officials').get()
-    .then((snapshot) => {
-      // let docs = getDocuments(snapshot);
-      // console.log(docs);
-      res.status(200).json(getDocuments(snapshot));
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-      handleError(res, error.message, "Failed to get officials.");
-    });
+  .then((snapshot) => {
+    // let docs = getDocuments(snapshot);
+    // console.log(docs);
+    res.status(200).json(getDocuments(snapshot));
+  })
+  .catch((error) => {
+    res.status(500).send(error);
+    handleError(res, error.message, "Failed to get officials.");
+  });
 });
+
+/**
+ * POST request for elected_officials
+ */
+app.post("/api/officials", function(req, res) {
+  var name = Object.keys(req.body)[0]
+  if (!name || !req.body[name].email || !req.body[name].role || !req.body[name].zip_codes) {
+    handleError(res, "Invalid user input", "Must provide a name, email, role, and zip code(s).", 400);
+  } else {
+    var official_to_be_added = {
+      email: req.body[name].email,
+      role: req.body[name].role,
+      zip_codes: req.body[name].zip_codes,
+    };
+    db.collection('elected_officials').doc(name).set(official_to_be_added, {merge: true})
+      .then(success => {
+        res.status(201).json({
+          success: true,
+          message: "Elected official successfully added.",
+        })
+      }, rejection => {
+        res.status(500).json({
+          success: false,
+          message: "Elected official failed to be added.",
+        })
+      })
+      .catch(error => {
+        res.status(500).send(error);
+        handleError(res, error.message, "Failed to post official.")
+      });
+  }
+});
+//curl -d '{"test_official":{"email":"email1","role":"role1","zip_codes": {"zip1":true}}}' -H 'Content-Type: application/json' http://127.0.0.1:3000/api/news
 
 /**
  * GET request for news_stories
  */
 app.get("/api/news", function (req, res) {
   db.collection('news_stories').get()
-    .then((snapshot) => {
-      // console.log(snapshot.data());
-      res.status(200).json(getDocuments(snapshot));
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-      handleError(res, error.message, "Failed to get news.");
-    });
+  .then((snapshot) => {
+    // console.log(snapshot.data());
+    res.status(200).json(getDocuments(snapshot));
+  })
+  .catch((error) => {
+    res.status(500).send(error);
+    handleError(res, error.message, "Failed to get news.");
+  });
 });
+
+/**
+ * POST request for news_stories
+ */
+app.post("/api/news", function(req, res) {
+  var story_to_be_added = req.body;
+  if (!req.body.id || !req.body.headline || !req.body.body || !req.body.zip_code || !req.body.link) {
+    handleError(res, "Invalid user input", "Must provide an ID, headline, body, zip code(s), and link.", 400);
+  } else {
+    db.collection('news_stories').doc(story_to_be_added.id).set(story_to_be_added, {merge: true})
+      .then(success => {
+        res.status(201).json({
+          success: true,
+          message: "News story successfully added.",
+        })
+      }, rejection => {
+        res.status(500).json({
+          success: false,
+          message: "News story failed to be added.",
+        })
+      })
+      .catch(error => {
+        res.status(500).send(error);
+        handleError(res, error.message, "Failed to post news story.")
+      });
+  }
+});
+//curl -d '{"id": "uuid", "headline": "str","body": "str", "zip_code": {"array":true}, "link": "link"}' -H 'Content-Type: application/json' http://127.0.0.1:3000/api/news
 
 /**
  * GET request for zip_codes
  */
 app.get("/api/zip_codes", function (req, res) {
   db.collection('zip_codes').get()
-    .then((snapshot) => {
-      // console.log(snapshot.data());
-      res.status(200).json(getDocuments(snapshot));
+  .then((snapshot) => {
+    // console.log(snapshot.data());
+    res.status(200).json(getDocuments(snapshot));
+  })
+  .catch((error) => {
+    res.status(500).send(error);
+    handleError(res, error.message, "Failed to get zip codes.");
+  });
+});
+
+/**
+ * POST request for zip_codes
+ */
+app.post("/api/zip_codes", function(req, res) {
+  var zip_to_be_added = req.body;
+  if (!req.body) {
+    handleError(res, "Invalid user input", "Must provide zip code(s).", 400);
+  }
+  db.collection('zip_codes').doc('zip_codes_to_officials').update(zip_to_be_added)
+    .then(success => {
+      res.status(201).json({
+        success: true,
+        message: "Zip code successfully added.",
+      })
+    }, rejection => {
+      res.status(500).json({
+        success: false,
+        message: "Zip code failed to be added.",
+      })
     })
-    .catch((error) => {
+    .catch(error => {
       res.status(500).send(error);
-      handleError(res, error.message, "Failed to get zip codes.");
+      handleError(res, error.message, "Failed to post zip code.")
     });
 });
+// curl -d '{"33647": ["Kathy Castor", "Marco Rubio"]}' -H 'Content-Type: application/json' http://127.0.0.1:3000/api/zip_codes
