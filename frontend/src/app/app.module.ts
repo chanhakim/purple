a6b8e9973ed09a1c5ae1853dcfc7e1335c84e402
Quinnan Gill <quinnan.gill@gmail.com>
2021-02-13 00:03:54 -0500

    Added boilerplate

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateComponent } from './template/template.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
