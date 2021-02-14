c3597c0a8f636af29f3e5658e9606005c2b64fa9
chanhakim <chanhakim17@gmail.com>
2021-02-13 18:40:55 -0800

    adjusted ui for zipcode page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,21 @@
+const { gray } = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      black: colors.black,
+      gray: gray,
+      purple: {
+        light: '#E9E7FC',
+        dark: '#7F19E5'
+      }
+    }
   },
   variants: {
     extend: {},
